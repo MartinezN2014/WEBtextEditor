65bd7d3f6be8acd1f02dc7f654e7a5dfec2e092e
MartinezN2014 <2014nmartinez@gmail.com>
2023-10-18 21:43:27 -0400

    configured the webpack and added babel-Loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,85 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      // `hot` option enables HMR on the server.
+      hot: "only",
     },
     plugins: [
-      
+      // Webpack plugin that simplifies creation of HTML files to serve your webpack bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+        favicon: "./favicon.ico",
+      }),
+      // Webpack plugin that generates our service worker file
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "./src-sw.js",
+      }),
+
+      // Webpack plugin that generates our manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "A simple text editor that works offline.",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader is used to transpile ES6 code to ES5.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
